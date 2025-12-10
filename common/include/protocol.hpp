448870406c18243ba6b97c7d5ed5727490b59277
PiotrW01 <piotr140401@gmail.com>
2025-12-10 14:02:18 +0100

    [Common] Improve networking protocol

--- common/include/protocol.hpp
+++ common/include/protocol.hpp
@@ -6,27 +6,69 @@
 #include <vector>
 
 namespace Proto {
+using Payload     = std::vector<uint8_t>;
+using Bytes       = std::vector<uint8_t>;
+using PayloadSize = uint16_t;
 
 enum ID : uint8_t {
     LOGIN,
     MESSAGE,
+    ERROR_HEADER_SIZE = 100,
 };
 
+enum class Endianness { NETWORK_TO_HOST, HOST_TO_NETWORK };
+
+/**
+ * Proto::PacketHandler, lepszą nazwe wymyślić może
+ * function callback ktory otrzymuje hdr.id wraz z payload
+ * albo func callback dla kazdego rodzaju pakietu ktory otrzymuje
+ * gotowy skonstruowany pakiet z payload'u
+ *
+ * byłoby zamiast switch case od hdr.id w klience i na serverze
+ */
+
+#pragma pack(push, 1)
 struct PacketHeader {
-    Proto::ID id;
-    uint16_t  length;
-    PacketHeader(Proto::ID p_id, uint16_t p_length) {
-        id = p_id;
-        // host to network byte ordering (16bit)
-        length = htons(p_length);
-    };
+    Proto::ID   id;
+    PayloadSize length;
     PacketHeader() : id(), length(){};
+    PacketHeader(Proto::ID p_id, PayloadSize p_length, Endianness mode) {
+        id = p_id;
+        if (mode == Endianness::NETWORK_TO_HOST) {
+            length = ntohs(p_length);
+        } else {
+            length = htons(p_length);
+        }
+    };
+    PacketHeader(const Bytes &buffer, Endianness mode) {
+        if (buffer.size() < sizeof(PacketHeader)) {
+            id     = ID::ERROR_HEADER_SIZE;
+            length = 0;
+            return;
+        }
+
+        id = static_cast<ID>(buffer[0]);
+        if (mode == Endianness::NETWORK_TO_HOST) {
+            length = ntohs(*reinterpret_cast<const PayloadSize *>(buffer.data() + sizeof(ID)));
+        } else {
+            length = htons(*reinterpret_cast<const PayloadSize *>(buffer.data() + sizeof(ID)));
+        }
+    };
+
+    Payload serialize() const {
+        Payload buffer(3);
+        buffer[0]                  = static_cast<uint8_t>(id);
+        PayloadSize network_length = htons(length);
+        std::memcpy(buffer.data() + 1, &network_length, sizeof(network_length));
+        return buffer;
+    }
 };
+#pragma pack(pop)
 
 struct LoginRequest {
     std::string username;
 
-    std::vector<uint8_t> serialize() const {
+    Proto::Payload serialize() const {
         return std::vector<uint8_t>(username.begin(), username.end());
     }
 
@@ -43,8 +85,8 @@ struct Message {
     std::string username;
     std::string message;
 
-    std::vector<uint8_t> serialize() const {
-        std::vector<uint8_t> payload;
+    Payload serialize() const {
+        Payload payload;
 
         uint8_t username_Length = static_cast<uint8_t>(std::min<size_t>(username.size(), 255));
         payload.push_back(username_Length);
@@ -54,8 +96,8 @@ struct Message {
         return payload;
     }
 
-    static std::vector<uint8_t> serialize(const std::string &username, const std::string &message) {
-        std::vector<uint8_t> payload;
+    static Payload serialize(const std::string &username, const std::string &message) {
+        Payload payload;
 
         uint8_t username_length = static_cast<uint8_t>(std::min<size_t>(username.size(), 255));
         payload.push_back(username_length);
@@ -65,7 +107,7 @@ struct Message {
         return payload;
     }
 
-    static Message deserialize(const std::vector<uint8_t> &data) {
+    static Message deserialize(const Payload &data) {
         Message msg;
 
         if (data.empty())
@@ -80,5 +122,51 @@ struct Message {
         return msg;
     }
 };
+
+inline void send_packet(const int &fd, const Payload payload, Proto::ID packet_id) {
+    PacketHeader hdr(packet_id, payload.size(), Proto::Endianness::HOST_TO_NETWORK);
+    send(fd, &hdr, sizeof(hdr), 0);
+    send(fd, payload.data(), payload.size(), 0);
+}
+
+inline void send_packet(const int &fd, const Payload payload, const PacketHeader &hdr) {
+    send(fd, &hdr, sizeof(hdr), 0);
+    send(fd, payload.data(), payload.size(), 0);
+}
+
+/**
+ * @brief
+ *
+ * @param fd
+ * @param buffer
+ * @return Returns -1 for errors or 0 if the connection closed. Otherwise
+ * returns the number of bytes received.
+ */
+inline int receive_data(const int &fd, Bytes &buffer) {
+    Bytes temp(8192);
+    int   bytes_received = recv(fd, temp.data(), temp.size(), 0);
+    if (bytes_received <= 0) {
+        return bytes_received;
+    }
+    buffer.insert(buffer.end(), temp.begin(), temp.begin() + bytes_received);
+    return bytes_received;
+}
+
+inline bool is_header_ready(const Bytes &buffer) {
+    return (buffer.size() >= sizeof(PacketHeader));
+}
+
+inline bool is_packet_ready(const Bytes &buffer, const PayloadSize &payload_size) {
+    return buffer.size() >= sizeof(PacketHeader) + payload_size;
+}
+
+inline Payload get_payload(const Bytes &buffer, const PayloadSize &payload_size) {
+    return Payload(buffer.begin() + sizeof(PacketHeader),
+                   buffer.begin() + sizeof(PacketHeader) + payload_size);
+}
+
+inline void remove_packet(Bytes &buffer, const PayloadSize payload_size) {
+    buffer.erase(buffer.begin(), buffer.begin() + sizeof(PacketHeader) + payload_size);
+}
 
 } // namespace Proto
