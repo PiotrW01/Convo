4013dcad861cc62723d3afe72787daa89cae6f52
PiotrW01 <piotr140401@gmail.com>
2025-12-17 22:27:02 +0100

    Move build info to common

--- common/include/build_info.hpp
+++ common/include/build_info.hpp
@@ -7,3 +7,11 @@
 #ifndef SERVER_VERSION
 #define SERVER_VERSION "0.0.1"
 #endif
+
+#ifndef CLIENT_NAME
+#define CLIENT_NAME "ConvoClient"
+#endif
+
+#ifndef CLIENT_VERSION
+#define CLIENT_VERSION "0.0.1"
+#endif
